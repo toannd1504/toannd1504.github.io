5aa1d1893f1b82d5655728fdf613b91a64f9394a
toantinhte2000 <toanyeuchuyenhalong123@gmail.com>
2026-01-03 22:17:58 +0700

    Skip loading page and show welcome screen immediately

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -306,11 +306,9 @@ export const guest = (() => {
             document.getElementById('information')?.remove();
         }
 
-        // wait until welcome screen is show.
-        await util.changeOpacity(document.getElementById('welcome'), true);
-
-        // remove loading screen and show welcome screen.
-        await util.changeOpacity(document.getElementById('loading'), false).then((el) => el.remove());
+        // Skip loading and show welcome screen immediately
+        document.getElementById('loading')?.remove();
+        document.getElementById('welcome').style.opacity = '1';
     };
 
     /**
