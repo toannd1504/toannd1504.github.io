ad10c58d26fab12eb9dc0e5e9403033baa447adc
toantinhte2000 <toanyeuchuyenhalong123@gmail.com>
2026-01-10 20:38:47 +0700

    update

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -256,10 +256,10 @@ export const guest = (() => {
         const url = new URL('https://calendar.google.com/calendar/render');
         const data = new URLSearchParams({
             action: 'TEMPLATE',
-            text: 'The Wedding of Wahyu and Riski',
-            dates: `${formatDate('2023-03-15 10:00')}/${formatDate('2023-03-15 11:00')}`,
-            details: 'Tanpa mengurangi rasa hormat, kami mengundang Anda untuk berkenan menghadiri acara pernikahan kami. Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.',
-            location: 'RT 10 RW 02, Desa Pajerukan, Kec. Kalibagor, Kab. Banyumas, Jawa Tengah 53191.',
+            text: 'Đám cưới Thoan và Toàn',
+            dates: `${formatDate('2026-02-07 09:15')}/${formatDate('2026-02-07 10:15')}`,
+            details: 'Tiệc cưới.',
+            location: 'Nhà hàng Thiên Tân Palace, Cái Dăm, Bãi Cháy, Quảng Ninh .',
             ctz: config.get('tz'),
         });
 
